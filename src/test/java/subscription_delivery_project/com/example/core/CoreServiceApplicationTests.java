12f3b0ef620df07ab3b5e870f14783b12ab0be50
YashSomani18 <yash@finvestfx.com>
2025-10-19 22:36:27 +0530

    Initial commit: Core service setup

---
+++ src/test/java/subscription_delivery_project/com/example/core/CoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package subscription_delivery_project.com.example.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
